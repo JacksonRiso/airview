ff45c5f0e905811202fe5bba4d98bbdfe7b9ad65
jacksonriso <risojackson@gmail.com>
2026-08-18 15:53:27 -0700

    Better rubocop

--- app/helpers/airview/application_helper.rb
+++ app/helpers/airview/application_helper.rb
@@ -135,9 +135,23 @@ module Airview
                 when :boolean
                   [["is checked", "is_true"], ["is unchecked", "is_false"]]
                 when :integer, :float, :decimal
-                  [["=", "equals"], [">", "gt"], ["<", "lt"], [">=", "gte"], ["<=", "lte"], ["is empty", "is_empty"], ["is not empty", "is_not_empty"]]
+                  [
+                    ["=", "equals"],
+                    [">", "gt"],
+                    ["<", "lt"],
+                    [">=", "gte"],
+                    ["<=", "lte"],
+                    ["is empty", "is_empty"],
+                    ["is not empty", "is_not_empty"]
+                  ]
                 when :date, :datetime
-                  [["is", "equals"], ["before", "before"], ["after", "after"], ["is empty", "is_empty"], ["is not empty", "is_not_empty"]]
+                  [
+                    ["is", "equals"],
+                    ["before", "before"],
+                    ["after", "after"],
+                    ["is empty", "is_empty"],
+                    ["is not empty", "is_not_empty"]
+                  ]
                 else
                   [
                     ["contains", "contains"],
